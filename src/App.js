9ba9a457471cc9773360b282786d3e13a241eb76
juverasworld <108337782+juverasworld@users.noreply.github.com>
2023-02-14 12:49:01 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             <div className='flex mb-4 px-4 '><p className='pr-5 my-2'><img src={message} className='w-10 h-10 mt-4 animate-pulse' /></p><input type='email' className='w-full mb-4' placeholder='Email' required /></div>
             <div className='flex  px-4 '><p className='pr-5 my-2'><img src={message} className='w-10 h-10 animate-ping' /></p>
               <input type='text' className='w-full  mb-4' required /> </div>
-            <div className='items-end    ml-44 sm:ml-48 my-10 '> <button className='border-2 sm:mr-10 mr-5 rounded-full px-8 py-2  bg-purple-900'>Send</button></div>
+            <div className='items-end    ml-44 sm:ml-48 my-10 '> <button className='border-2 sm:mr-10 mr-5 rounded-full px-8 py-2  bg-purple-400'>Send</button></div>
           </div>
 
         </form>
