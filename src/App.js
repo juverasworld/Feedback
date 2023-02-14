d09d6ed145d385b15e970ec4b170e57c39ed5a57
juverasworld <108337782+juverasworld@users.noreply.github.com>
2023-02-14 12:37:21 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import user from './user.jpg';
+import message from "./message.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <div className="bg-purple-400 w-full h-screen items-center text-center flex flex-col sm:px-auto px-5 ">
+        <div className='mt-32 text-white ]' >
+          <p className='font-extrabold text-[42px]'>Feedback</p>
+          <p className='mb-3 animate-pulse font-bold'>Should you have face any issues. feel free to contact  <br className='sm:block hidden'/> we would get back to you as soon as we can !</p></div>
+        <form className='w-[100%]rounded-2xl sm:px-auto px-5'>
+          <div className=' w-[100%] bg-white mb-20 rounded-2xl'>
+            <div className='flex  px-4 '>  <p className='pr-5 my-2'><img src={user} className='w-10 h-10 mt-4 animate-bounce' /></p>
+              <input type='text' placeholder='Name' className='border-b-black w-full mb-4 required  ' />
+            </div>
+            <div className='flex mb-4 px-4 '><p className='pr-5 my-2'><img src={message} className='w-10 h-10 mt-4 animate-pulse' /></p><input type='email' className='w-full mb-4' placeholder='Email' required /></div>
+            <div className='flex  px-4 '><p className='pr-5 my-2'><img src={message} className='w-10 h-10 animate-ping' /></p>
+              <input type='text' className='w-full  mb-4' required /> </div>
+            <div className='items-end    ml-44 sm:ml-48 my-10 '> <button className='border-2 sm:mr-10 mr-5 rounded-full px-8 py-2  bg-purple-900'>Send</button></div>
+          </div>
+
+        </form>
+      </div>
+
     </div>
   );
 }
